abe6d87354fe0fd9c4d133f343c58733efe34b97
John Stuart Phillips <0q5av15l@students.codeinstitute.net>
2024-10-12 17:08:37 +0000

    Add results and score functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,14 +11,28 @@ const messages = document.getElementById("messages");
 const choices = ["rock", "paper", "scissors"];
 
 
-/**
- * Add event listener to all the buttons
- */
-for (let button of buttons) {
-    button.addEventListener("click", function () {
-        let playerChoice = this.getAttribute("data-choice");
-        playGame(playerChoice);
-    });
+function addListenersToControlButtons() {
+    /**
+     * Add event listener to all the buttons
+     */
+    for (let button of buttons) {
+        // button.addEventListener("click", function () {
+        //     let playerChoice = this.getAttribute("data-choice");
+        //     playGame(playerChoice);
+        // });
+        button.addEventListener("click", getPlayerChoice)
+    }
+}
+
+function getPlayerChoice(event) {
+    let playerChoice = event.target.getAttribute("data-choice");
+    playGame(playerChoice);
+} 
+
+function removeListenersFromControlButtons() {
+    for (let button of buttons) {
+        button.removeEventListener("click", getPlayerChoice)
+    }
 }
 
 /**
@@ -26,6 +40,8 @@ for (let button of buttons) {
  * is the data-choice value of the selected button
  */
 function playGame(playerChoice) {
+
+    removeListenersFromControlButtons()
 
     playerImage.src = `assets/images/${choices[playerChoice]}.png`;
     playerImage.alt = choices[playerChoice];
@@ -37,5 +53,56 @@ function playGame(playerChoice) {
 
     let result = checkWinner(choices[computerChoice], choices[playerChoice]);
 
+    updateMessage(result);
+
     updateScores(result);
+
+    setTimeout(addListenersToControlButtons, 500)
 }
+
+function checkWinner(computerChoice, playerChoice) {
+    if (computerChoice == playerChoice) {
+        return "draw"
+    } else if (playerChoice == "rock" && computerChoice == "scissors") {
+        return "player"
+    } else if (playerChoice == "rock" && computerChoice == "paper") {
+        return "computer"
+    } else if (playerChoice == "paper" && computerChoice == "rock") {
+        return "player"
+    } else if (playerChoice == "paper" && computerChoice == "scissors") {
+        return "computer"
+    } else if (playerChoice == "scissors" && computerChoice == "paper") {
+        return "player"
+    } else if (playerChoice == "scissors" && computerChoice == "rock") {
+        return "computer"
+    }
+}
+
+function updateScores(result) {
+    if (result == "draw") {
+        return
+    }
+
+    let scoreSpan = document.getElementById(result + "-score");
+    let score = scoreSpan.innerHTML
+    score++
+    scoreSpan.innerHTML = score
+
+
+}
+
+function updateMessage(result) {
+    let message = ""
+    if (result == "draw") {
+        message = "It's a draw"
+    } else if (result == "player") {
+        message = "Player Wins"
+    } else {
+        message = "Computer Wins"
+    }
+
+    let messagesDiv = document.getElementById("messages");
+    messagesDiv.innerHTML = message;
+}
+
+addListenersToControlButtons();
