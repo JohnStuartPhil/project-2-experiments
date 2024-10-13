04bb83a34e8cb55e6615ab735efcab3067da16d6
John Stuart Phillips <0q5av15l@students.codeinstitute.net>
2024-10-13 17:09:06 +0000

    Amend modal button text

--- assets/js/script.js
+++ assets/js/script.js
@@ -159,17 +159,19 @@ var span = document.getElementsByClassName("close")[0];
 btn.onclick = function() {
   modal.style.display = "block";
   let modalButton = document.getElementById('myBtn');
-  modalButton.textContent = "Click anywhere to close";
+  modalButton.textContent = "Click x to close";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+  let modalButton = document.getElementById('myBtn');
+  modalButton.textContent = "Click here for instructions";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+//window.onclick = function(event) {
+  //if (event.target == modal) {
+   // modal.style.display = "none";
+  //}
+//}
